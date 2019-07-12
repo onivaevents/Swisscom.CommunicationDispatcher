0365f91b3ef87560c09095049acb21be72c0861d
Simon Gadient <simon.gadient@swisscom.com>
2019-07-12 21:40:59 +0200

    [TASK] Adapt migration scripts to upgraded Flow 5.3 version

--- Migrations/Mysql/Version20171212225913.php
+++ Migrations/Mysql/Version20171212225913.php
@@ -26,8 +26,8 @@ class Version20171212225913 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on "mysql".');
         
-        $this->addSql('CREATE TABLE swisscom_communicationdispatcher_domain_model_notification (persistence_object_identifier VARCHAR(40) NOT NULL, person VARCHAR(40) DEFAULT NULL, subject VARCHAR(255) NOT NULL, text VARCHAR(255) NOT NULL, isread TINYINT(1) NOT NULL, timestamp DATETIME NOT NULL, INDEX IDX_C9B1358A34DCD176 (person), PRIMARY KEY(persistence_object_identifier)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE swisscom_communicationdispatcher_domain_model_notification ADD CONSTRAINT FK_C9B1358A34DCD176 FOREIGN KEY (person) REFERENCES typo3_party_domain_model_person (persistence_object_identifier) ON DELETE CASCADE');
+        $this->addSql('CREATE TABLE swisscom_communicationdispatcher_domain_model_notification (persistence_object_identifier VARCHAR(40) NOT NULL, person VARCHAR(40) DEFAULT NULL, subject VARCHAR(255) NOT NULL, text VARCHAR(255) NOT NULL, isread TINYINT(1) NOT NULL, timestamp DATETIME NOT NULL, INDEX IDX_C9B1358A34DCD176 (person), PRIMARY KEY(persistence_object_identifier)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE swisscom_communicationdispatcher_domain_model_notification ADD CONSTRAINT FK_C9B1358A34DCD176 FOREIGN KEY (person) REFERENCES neos_party_domain_model_person (persistence_object_identifier) ON DELETE CASCADE');
     }
 
     /**
